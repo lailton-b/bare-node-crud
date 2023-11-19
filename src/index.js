9a7e91b5f76872ad462c8c01edab264b52215a47
Lailton <lailton_b@hotmail.com>
2023-11-19 11:51:47 -0300

    feat: v1

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const users = require('./mocks/users');
+const { URL } = require('url');
+
+const bodyParser = require('./helpers/bodyParser');
+const routes = require('../routes');
+
+const server = http.createServer((request, response) => {
+    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+
+    const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+    let { pathname } = parsedUrl;
+    let id = null;
+
+    const splitEndpoint = pathname.split('/').filter(Boolean);
+
+    if (splitEndpoint.length > 1) {
+        pathname = `/${splitEndpoint[0]}/:id`;
+        id = splitEndpoint[1];
+    }
+
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === pathname && routeObj.method === request.method
+    ))
+
+    if (route) {
+        request.query = Object.fromEntries(parsedUrl.searchParams);
+        request.params = { id };
+        response.send = (statusCode, body) => {
+            response.writeHead(statusCode, { 'Content-Type': 'text/html' });
+            response.end(JSON.stringify(body));
+        }
+       
+        if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+            bodyParser(request, () => route.handler(request, response));
+        } else {
+            route.handler(request, response);
+        }
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end(`Cannot ${request.method} ${request.url}`)
+    }
+})
+
+server.listen(3000, () => console.log(`Server started at http://localhost:3000`))
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'User One'
+    },
+    {
+        id: 2,
+        name: 'User Two'
+    },
+    {
+        id: 3,
+        name: 'User Three'
+    }
+]
